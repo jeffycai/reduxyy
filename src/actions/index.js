63e10ec6fefd45d4ba71c6791933b3ac95e32889
cxb <cxb@rrtimes.com>
2017-08-14 19:34:48 +0800

    ddddddddd

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const additionAChange = (e) => ({
-    type: 'ADDITION_A_CHANGE',
-    value:e.target.value
-})
-
-export const subtractionAChange = (e) => ({
-    type: 'SUBTRACTION_A_CHANGE',
-    value:e.target.value
-})
